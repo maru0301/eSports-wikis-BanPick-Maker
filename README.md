d914e27e914942ae76158559844bf4d6f2631727
maru0301 <sp7p4z69@helen.ocn.ne.jp>
2017-02-07 14:21:04 +0900

    add

---
+++ README.md
@@ -0,0 +1,17 @@
+# Ban-Pick-Code-Maker
+
+e-Sports wikisのBan Pickページを編集するWebページ
+プルダウンメニューからチャンピオンを選択し、下部にあるボタンを押すとBan Pick wikiのタグが出力される
+
+シーズン2017対応
+
+[非対応]
+勝敗
+Week/Round
+
+---------------------------------------
+
+e-Sports wikis Ban Pick output Tag Web page
+
+Season 2017 compatible
+
---
+++ php/main.php
@@ -0,0 +1,52 @@
+<?php
+
+if( !isset( $_GET['func'] ) ) return;
+
+//-------------------------------------------------
+
+class RiotApi
+{
+	private $api_key = '561cb811-20fb-4e50-bc7f-3f3c8cd345e4';
+	
+	private function GetJson( $url )
+	{
+		$master_url = $url . $this->api_key;
+		$json = file_get_contents($master_url);
+		$json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+		
+		return $json;
+	}
+	
+	public function GetVersion()
+	{
+		$json = $this->GetJson('https://global.api.pvp.net/api/lol/static-data/jp/v1.2/realm?api_key=');
+		
+		return $json;
+	}
+	
+	public function GetChampionImage()
+	{
+		$json = $this->GetJson('https://global.api.pvp.net/api/lol/static-data/jp/v1.2/champion?champData=image&api_key=');
+		
+		return $json;
+	}
+}
+
+//-------------------------------------------------
+
+$api = new RiotApi;
+
+$func_tbl = array(
+			"GetVersion" => "GetVersion",
+			"GetChampionImage" => "GetChampionImage",
+);
+
+//-------------------------------------------------
+
+$func_name = $_GET['func'];
+
+echo $api->{$func_tbl[$func_name]}();
+
+//-------------------------------------------------
+
+?>
